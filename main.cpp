dad77d5c5d809aae0139609e8172d40d71fbb87b
max <max@thousandyardstare.de>
2018-12-18 16:31:27 +0100

    extracted raw pixels to opengl to own function

--- main.cpp
+++ main.cpp
@@ -35,34 +35,45 @@ std::string strval = "A string";
 test_enum enumval = Item2;
 Color colval(0.5f, 0.5f, 0.7f, 1.f);
 
+int getTextureForPixels(float pixels[], int width, int height) {
+    GLuint imageTexId;
+    glGenTextures(1, &imageTexId);
+    glBindTexture(GL_TEXTURE_2D, imageTexId);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_FLOAT, pixels);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    return imageTexId;
+}
+
 int main(int /* argc */, char ** /* argv */) {
 
     // First, load an image with openCV and do stuff with it
-//    cv::Mat image;
-//    image = cv::imread("./data/rgbd_dataset_freiburg3_teddy/rgb/1341841873.273798.png", CV_LOAD_IMAGE_COLOR);   // Read the file
-//
-//    if(! image.data )                              // Check for invalid input
-//    {
-//        std::cout <<  "Could not open or find the image" << std::endl ;
-//        return -1;
-//    }
-//
-//    namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
-//    imshow( "Display window", image );                   // Show our image inside it.
-//
-//    cv::waitKey(0);
+    cv::Mat image;
+    image = cv::imread("./data/rgbd_dataset_freiburg3_teddy/rgb/1341841873.273798.png", CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    cv::waitKey(0);
 
     // Second, display the image using nano
     nanogui::init();
     Screen *screen = new Screen(Vector2i(500, 700), "NanoGUI test");
 
-    auto imageWindow = new Window(screen, "Selected image");
+    auto imageWindow = new Window(screen, "RGB Left");
     imageWindow->setPosition({50, 50});
     imageWindow->setLayout(new GroupLayout());
-
-    GLuint imageTexId;
-    glGenTextures(1, &imageTexId);
-    glBindTexture(GL_TEXTURE_2D, imageTexId);
 
     int width = 48;
     int height = 48;
@@ -72,12 +83,7 @@ int main(int /* argc */, char ** /* argv */) {
         pixels[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
     }
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_FLOAT, pixels);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    int imageTexId = getTextureForPixels(pixels, width, height);
 
     auto imageView = new ImageView(imageWindow, imageTexId);
     imageView->setSize({100,100});
@@ -91,59 +97,5 @@ int main(int /* argc */, char ** /* argv */) {
     nanogui::mainloop();
     nanogui::shutdown();
 
-    return 0;
-
-    nanogui::init();
-
-    /* scoped variables */ {
-        bool use_gl_4_1 = false;// Set to true to create an OpenGL 4.1 context.
-        Screen *screen = nullptr;
-
-        if (use_gl_4_1) {
-            // NanoGUI presents many options for you to utilize at your discretion.
-            // See include/nanogui/screen.h for what all of these represent.
-            screen = new Screen(Vector2i(500, 700), "NanoGUI test [GL 4.1]",
-                    /*resizable*/true, /*fullscreen*/false, /*colorBits*/8,
-                    /*alphaBits*/8, /*depthBits*/24, /*stencilBits*/8,
-                    /*nSamples*/0, /*glMajor*/4, /*glMinor*/1);
-        } else {
-            screen = new Screen(Vector2i(500, 700), "NanoGUI test");
-        }
-
-        bool enabled = true;
-        FormHelper *gui = new FormHelper(screen);
-        ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-        gui->addGroup("Basic types");
-        gui->addVariable("bool", bvar);
-        gui->addVariable("string", strval);
-
-        gui->addGroup("Validating fields");
-        gui->addVariable("int", ivar)->setSpinnable(true);
-        gui->addVariable("float", fvar);
-        gui->addVariable("double", dvar)->setSpinnable(true);
-
-        gui->addGroup("Complex types");
-        gui->addVariable("Enumeration", enumval, enabled)
-                ->setItems({"Item 1", "Item 2", "Item 3"});
-        gui->addVariable("Color", colval)
-                ->setFinalCallback([](const Color &c) {
-                    std::cout << "ColorPicker Final Callback: ["
-                              << c.r() << ", "
-                              << c.g() << ", "
-                              << c.b() << ", "
-                              << c.w() << "]" << std::endl;
-                });
-
-        gui->addGroup("Other widgets");
-        gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; });
-
-        screen->setVisible(true);
-        screen->performLayout();
-        window->center();
-
-        nanogui::mainloop();
-    }
-
-    nanogui::shutdown();
     return 0;
 }
