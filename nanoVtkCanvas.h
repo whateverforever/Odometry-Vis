aa4a1f84d3d893faca08a88f54c6fa45cd8851bb
max <max@thousandyardstare.de>
2018-12-21 13:24:25 +0100

    replaced old, non functioning opengl code with gl3 code

--- nanoVtkCanvas.h
+++ nanoVtkCanvas.h
@@ -10,6 +10,7 @@
 
 #include <nanogui/widget.h>
 #include <nanogui/glcanvas.h>
+#include <nanogui/opengl.h>
 
 #include <vtkPolyDataMapper.h>
 #include <vtkCommand.h>
@@ -23,19 +24,23 @@
 #include <vtkNew.h>
 #include <vtkPolyDataMapper.h>
 
+#include <Eigen/Core>
+
+using Vector3f = Eigen::Vector3f;
+
 class NanoVtkCanvas : public nanogui::GLCanvas {
 public:
     NanoVtkCanvas(Widget *parent) : nanogui::GLCanvas(parent) {
         if (!m_initialized) {
             vtkNew<vtkExternalOpenGLRenderWindow> renWin;
 
-            externalVTKWidget->SetRenderWindow(renWin);
+            m_externalVTKWidget->SetRenderWindow(renWin);
 
             vtkNew<vtkPolyDataMapper> mapper;
             vtkNew<vtkActor> actor;
             actor->SetMapper(mapper);
 
-            vtkRenderer* ren = externalVTKWidget->AddRenderer();
+            vtkRenderer* ren = m_externalVTKWidget->AddRenderer();
             ren->AddActor(actor);
 
             vtkNew<vtkCubeSource> cs;
@@ -45,21 +50,33 @@ public:
             ren->ResetCamera();
 
             m_rawWindow = parent->screen()->glfwWindow();
-
             m_initialized = true;
         }
     }
 
     virtual void drawGL() override {
-        //glEnable(GL_DEPTH_TEST);
+        m_externalVTKWidget->GetRenderWindow()->Render();
 
-        externalVTKWidget->GetRenderWindow()->Render();
+        Vector3f Vertices[1];
+        Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
 
-        //glDisable(GL_DEPTH_TEST);
+        GLuint VBO;
+        glGenBuffers(1, &VBO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glDrawArrays(GL_POINTS, 0, 1);
+
+        glDisableVertexAttribArray(0);
     }
 
 private:
     bool m_initialized;
     GLFWwindow *m_rawWindow;
-    vtkNew<ExternalVTKWidget> externalVTKWidget;
+    vtkNew<ExternalVTKWidget> m_externalVTKWidget;
 };
