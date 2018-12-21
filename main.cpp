f62c429ab62b728df66328cbf154c876f47a4b42
max <max@thousandyardstare.de>
2018-12-21 13:24:52 +0100

    fixed gitignore and main for sublime workflow

--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ int main(int /* argc */, char ** /* argv */) {
 
     // First, load an image with openCV and do stuff with it
     cv::Mat image, image_2;
-    image = cv::imread("./data/rgbd_dataset_freiburg3_teddy/rgb/1341841873.273798.png", CV_LOAD_IMAGE_COLOR);   // Read the file
-    image_2 = cv::imread("./data/rgbd_dataset_freiburg3_teddy/rgb/1341841879.367159.png", CV_LOAD_IMAGE_COLOR);   // Read the file
+    image = cv::imread("../data/rgbd_dataset_freiburg3_teddy/rgb/1341841873.273798.png", CV_LOAD_IMAGE_COLOR);   // Read the file
+    image_2 = cv::imread("../data/rgbd_dataset_freiburg3_teddy/rgb/1341841879.367159.png", CV_LOAD_IMAGE_COLOR);   // Read the file
 
     if(! image.data )                              // Check for invalid input
     {
