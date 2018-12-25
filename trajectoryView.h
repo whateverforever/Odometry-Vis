2a2c25159004277426b520894528b801b6c353ad
max <max@thousandyardstare.de>
2018-12-26 00:41:57 +0100

    removed colors to simplify

--- trajectoryView.h
+++ trajectoryView.h
@@ -26,10 +26,9 @@ public:
             "#version 330\n"
             "uniform mat4 modelViewProj;\n"
             "in vec3 position;\n"
-            "in vec3 color;\n"
             "out vec4 frag_color;\n"
             "void main() {\n"
-            "    frag_color = 3.0 * modelViewProj * vec4(color, 1.0);\n"
+            "    frag_color = vec4(0.8, 0.8, 0.8, 1.0);\n"
             "    gl_Position = modelViewProj * vec4(position, 1.0);\n"
             "}",
 
@@ -52,24 +51,9 @@ public:
         positions.col(6) <<  1, -1, -1;
         positions.col(7) <<  1, -1,  1;
 
-        MatrixXf colors(3, 12);
-        colors.col( 0) << 1, 0, 0;
-        colors.col( 1) << 0, 1, 0;
-        colors.col( 2) << 1, 1, 0;
-        colors.col( 3) << 0, 0, 1;
-        colors.col( 4) << 1, 0, 1;
-        colors.col( 5) << 0, 1, 1;
-        colors.col( 6) << 1, 1, 1;
-        colors.col( 7) << 0.5, 0.5, 0.5;
-        colors.col( 8) << 1, 0, 0.5;
-        colors.col( 9) << 1, 0.5, 0;
-        colors.col(10) << 0.5, 1, 0;
-        colors.col(11) << 0.5, 1, 0.5;
-
         mShader.bind();
 
         mShader.uploadAttrib("position", positions);
-        mShader.uploadAttrib("color", colors);
     }
 
     ~TrajectoryView() {
