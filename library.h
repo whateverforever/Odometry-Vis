6d3ee614f34252e97e1c3768040fc6d681171310
max <max@thousandyardstare.de>
2018-12-26 13:03:47 +0100

    moved headers from cpp to h

--- library.h
+++ library.h
@@ -1,11 +1,23 @@
 #ifndef VISUALIZATION_LIBRARY_H
 #define VISUALIZATION_LIBRARY_H
 
+#include <iostream>
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+
+#include <nanogui/nanogui.h>
+#include <nanogui/imagepanel.h>
+#include <nanogui/imageview.h>
+
 class Vis {
 public:
     Vis();
     void hello();
     void what();
+private:
+    std::vector<&cv::Mat> m_frames;
+    nanogui::MatrixXf m_trajectoryPoints;
 };
 
 #endif
