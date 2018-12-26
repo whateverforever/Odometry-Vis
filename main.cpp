38aca205e4ff5258aa7e5a476778e5eff8436331
max <max@thousandyardstare.de>
2018-12-26 13:17:30 +0100

    include the needed opencv libs

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
-#include "trajectoryView.h"
 #include "library.h"
-
-using namespace nanogui;
 
 int main(int /* argc */, char ** /* argv */) {
 
