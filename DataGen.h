b4efb1a1fe2e59ae0d8c4fb2811ee60f9325764b
max <max@thousandyardstare.de>
2018-12-27 13:17:45 +0100

    added class for generating data

---
+++ DataGen.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "library.h"
+
+class DataGenerator {
+public:
+    DataGenerator();
+    void updateValue();
+    friend class Vis;
+private:
+    int m_someValue;
+};
+
+DataGenerator::DataGenerator() {
+    m_someValue = 555;
+}
+
+void DataGenerator::updateValue() {
+    m_someValue += 10;
+}
