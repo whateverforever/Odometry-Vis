c89dcc9d95be1a295ea7974b14b394abae1b4c35
max <max@thousandyardstare.de>
2019-01-16 17:54:38 +0100

    added a new shader for the camera symbol

--- src/TrajectoryView.h
+++ src/TrajectoryView.h
@@ -46,9 +46,37 @@ public:
 
     mShader.bind();
     mShader.uploadAttrib("position", m_positions);
+
+    m_camSymShader.init(
+        /* An identifying name */
+        "camera_symbol_shader",
+
+        /* Vertex shader */
+        "#version 330\n"
+        "uniform mat4 modelViewProj;\n"
+        "in vec3 position;\n"
+        "out vec4 frag_color;\n"
+        "void main() {\n"
+        "    frag_color = vec4(0.5, 0.5, 1.0, 1.0);\n"
+        "    gl_Position = modelViewProj * vec4(position, 1.0);\n"
+        "}",
+
+        /* Fragment shader */
+        "#version 330\n"
+        "out vec4 color;\n"
+        "in vec4 frag_color;\n"
+        "void main() {\n"
+        "    color = frag_color;\n"
+        "}");
+
+    m_camSymShader.bind();
+    m_camSymShader.uploadAttrib("position", m_positions);
   }
 
-  ~TrajectoryView() { mShader.free(); }
+  ~TrajectoryView() {
+    mShader.free();
+    m_camSymShader.free();
+}
 
   void setRotation(nanogui::Vector3f vRotation) { mRotation = vRotation; }
 
@@ -68,8 +96,6 @@ public:
   virtual void drawGL() override {
     using namespace nanogui;
 
-    mShader.bind();
-
     Matrix4f mvp;
     mvp.setIdentity();
     float fTime = (float)glfwGetTime();
@@ -80,16 +106,24 @@ public:
             Eigen::AngleAxisf(mRotation[2] * fTime, Vector3f::UnitZ())) *
         0.25f;
 
+    mShader.bind();
     mShader.setUniform("modelViewProj", mvp);
 
     glEnable(GL_DEPTH_TEST);
-    /* Draw 12 triangles starting at index 0 */
     mShader.drawArray(GL_LINE_STRIP, 0, m_positions.cols() - 1);
+    glDisable(GL_DEPTH_TEST);
+
+
+    m_camSymShader.bind();
+    m_camSymShader.setUniform("modelViewProj", mvp);
+    glEnable(GL_DEPTH_TEST);
+    m_camSymShader.drawArray(GL_LINE, 0, m_positions.cols() - 1);
     glDisable(GL_DEPTH_TEST);
   }
 
 private:
   nanogui::MatrixXf m_positions;
   nanogui::GLShader mShader;
+  nanogui::GLShader m_camSymShader;
   Eigen::Vector3f mRotation;
 };
