2a0dcd4f2d6dff1ec75c50c8ce3d429cc3e14a10
max <max@thousandyardstare.de>
2019-01-16 16:07:32 +0100

    added data_types from wangs repo

---
+++ src/data_types.h
@@ -0,0 +1,21 @@
+#include <Eigen/Core>
+#include <opencv2/core.hpp>
+
+#ifndef PixelType
+#define PixelType CV_32F
+#endif
+
+namespace odometry
+{
+typedef Eigen::Matrix<float, 1, 2> RowVector2f;
+typedef Eigen::Vector2i Vector2i;
+typedef Eigen::Vector4f Vector4f;
+typedef Eigen::Matrix<float, 6, 1> Vector6f;
+typedef Eigen::Matrix3f Matrix33f;
+typedef Eigen::Matrix<float, 4, 4> Affine4f; // used as camera pose (R|T)
+typedef Eigen::Matrix<float, 2, 6> Matrix2ff;
+
+typedef int OptimizerStatus;
+typedef int GlobalStatus;
+
+} // namespace odometry
