417cf60d98895209c031bbd8715e4b556b36b70e
max <max@thousandyardstare.de>
2019-01-30 10:31:40 +0100

    replaced datagenerator with mockcamera in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,35 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 
-#include <Vis.h>
 #include <DataGen.h>
+#include <MockCameraInput.h>
+#include <Vis.h>
 
 int main(int /* argc */, char ** /* argv */) {
   auto myUI = new Vis();
   auto dataGenerator = new DataGenerator();
+  auto mockCamera = new MockCameraInput();
 
-  std::thread dataThread([dataGenerator, myUI]() {
+  std::thread dataThread([mockCamera, dataGenerator, myUI]() {
     while (true) {
-      // Getting the image from the camera, calculating the depth etc all happens here
-      // dataGenerator is basically the last link in the chain: Odometry
-      auto latestKeyframe = dataGenerator->getLatestKeyframe();
+      // Getting the image from the camera, calculating the depth etc all
+      // happens here dataGenerator is basically the last link in the chain:
+      // Odometry auto latestKeyframe = dataGenerator->getLatestKeyframe();
+      auto latestKeyframe = mockCamera->getLatestKeyframe();
 
       myUI->loadNewestKeyframe(latestKeyframe);
 
-      std::this_thread::sleep_for(
-          std::chrono::milliseconds(3000));
+      std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     }
   });
 
   myUI->start();
 
   dataThread.join();
+
+  delete myUI;
+  delete dataGenerator;
+  delete mockCamera;
 
   return 0;
 }
